f0ac4f99d9348d26fb2644f4bafa4b394e170bd3
George Emmanuel <rustydcoder@gmail.com>
2022-01-05 07:52:00 -0500

    folder-structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from "react";
+import {
+  Blog,
+  Features,
+  Footer,
+  WhatGPT3,
+  Possibility,
+  Header,
+} from "./containers";
+import { Cta, Brand, Navbar } from "./components";
+import "./app.css";
 
 const App = () => {
   return (
-    <div>
-      <h1>GPT-3</h1>
+    <div className="App">
+      <div className="gradient_bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <Cta />
+      <Blog />
+      <Footer />
     </div>
   );
 };
